6f9febb3358735c084903d928c0f1cec5d3aa565
gdepuydt <geert_depuydt@hotmail.com>
2019-07-24 13:56:20 +0200

    Matrix MUL functions

--- GraceAI/Matrix.h
+++ GraceAI/Matrix.h
@@ -3,7 +3,7 @@
 namespace GraceAI {
 
 	class Matrix {
-		double* data;
+		float* data;
 		int rows, columns;
 		int total;
 
@@ -12,12 +12,68 @@ namespace GraceAI {
 			this->rows = rows;
 			this->columns = columns;
 			this->total = rows * columns;
-			//align alloc to 64 bytes for cach line and AVX: performance reasons 
-			data = (double*)_aligned_malloc(sizeof(double) * total, 64);
+			//align alloc to 64 bytes for cache line size and AVX: performance reasons 
+			data = (float*)_aligned_malloc(sizeof(float) * total, 64);
 		}
 
 		~Matrix() {
 			_aligned_free(data);
+		}
+
+		float& operator() (int row, int col) {
+			return data[row + col * rows];
+		}
+
+		//Matrix dot product
+		void MUL(Matrix& a, Matrix& b) {
+			if (a.columns != b.rows) {
+				throw "Columns of matrix A must equal rows of matrix B";
+			}
+
+			if (this == &a || this == &b) {
+				throw "Destination matrix must be distinct from matrices A and B";
+			}
+
+			int sharedDim = a.columns;
+
+			for (int r = 0; r < a.rows; r++) {
+				for (int c = 0; c < b.columns; c++) {
+					float total = 0.0f;
+					for (int d = 0; d < sharedDim; d++) {
+
+						total += a(r, d) * b(d, c);
+					}
+
+					this->operator()(r, c) = total;
+				}
+			}
+		}
+
+		void Hadamard(Matrix& a) {
+			if (this->rows != a.rows || this->columns != a.columns) {
+				throw "Matrixes must be of same dimension to Hadamard!";
+			}
+			for (int i = 0; i < this->total; i++) {
+				data[i] *= a.data[i];
+			}
+		}
+
+		void ADD(Matrix& a) {
+			if (this->rows != a.rows || this->columns != a.columns) {
+				throw "Matrixes must be of same dimension to Add!";
+			}
+			for (int i = 0; i < this->total; i++) {
+				data[i] += a.data[i];
+			}
+		}
+
+		void SUB(Matrix& a) {
+			if (this->rows != a.rows || this->columns != a.columns) {
+				throw "Matrixes must be of same dimension to Sub!";
+			}
+			for (int i = 0; i < this->total; i++) {
+				data[i] -= a.data[i];
+			}
 		}
 	};
 }
